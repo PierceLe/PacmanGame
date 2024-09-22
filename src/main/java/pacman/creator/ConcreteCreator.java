63297305b57f31019794bc232936e8e465b29bc9
PierceLe <haichau.hvsg@gmail.com>
2024-09-23 04:43:27 +1000

    fix: fixing error of filePath

--- src/main/java/pacman/creator/ConcreteCreator.java
+++ src/main/java/pacman/creator/ConcreteCreator.java
@@ -12,47 +12,51 @@ public class ConcreteCreator implements Creator {
         if (rendererType == RenderableType.HORIZONTAL_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/horizontal.png"));
+            Image image = new Image(getClass().getResource("/maze/walls/horizontal.png").toExternalForm());
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, image);
         }
         if (rendererType == RenderableType.VERTICAL_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/vertical.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image(getClass().getResource("/maze/walls/vertical.png").toExternalForm()));
         }
         if (rendererType == RenderableType.DOWN_LEFT_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/down_left.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image(getClass().getResource("/maze/walls/downLeft.png").toExternalForm()));
         }
         if (rendererType == RenderableType.DOWN_RIGHT_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/down_right.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image(getClass().getResource("/maze/walls/downRight.png").toExternalForm()));
         }
         if (rendererType == RenderableType.UP_LEFT_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/up_left.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image(getClass().getResource("/maze/walls/upLeft.png").toExternalForm()));
         }
         if (rendererType == RenderableType.UP_RIGHT_WALL) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image("src/main/resources/maze/walls/up_right.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.BACKGROUND, new Image(getClass().getResource("/maze/walls/upRight.png").toExternalForm()));
         }
         if (rendererType == RenderableType.PELLET) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image("src/main/resources/maze/pellet.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image(getClass().getResource("/maze/pellet.png").toExternalForm()));
         }
         if (rendererType == RenderableType.GHOST) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 28, 28);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),28, 28);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image("src/main/resources/maze/ghosts/ghost.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image(getClass().getResource("/maze/ghosts/ghost.png").toExternalForm()));
         }
         if (rendererType == RenderableType.PACMAN) {
             Double[] topLeftCordinate = topLeftCoordinate(x, y, 16, 16);
             BoundingBoxImpl boundingBoxOfObject = new BoundingBoxImpl(new Vector2D(topLeftCordinate[0], topLeftCordinate[1]),16, 16);
-            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image("src/main/resources/maze/pacman/payerDown.png"));
+            return new StaticEntityImpl(boundingBoxOfObject, Renderable.Layer.FOREGROUND, new Image(getClass().getResource("/maze/pacman/playerDown.png").toExternalForm()));
+        }
+        if (rendererType == 0) {
+            return null;
         }
         return null;
     }
